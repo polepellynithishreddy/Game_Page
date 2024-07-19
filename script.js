dccbd8b234f49015142c138954fda6df29882e41
Polepelly Nithish Reddy <111244115+polepellynithishreddy@users.noreply.github.com>
2024-07-19 15:16:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,38 @@
 const gamesList = document.getElementById('games-list');
-
 // Add games list items
 const games = [
-	{ name: 'TIC-TAC-TOE', image: 'tic-tac-toe image.png', link: 'https://polepellynithishreddy.github.io/TIC-TAC-TOE/' },
-	{ name: 'CHECKERS', image: 'checkers image.png', link: 'https://polepellynithishreddy.github.io/Checkers-Game/' },
-	{ name: 'SOS', image: 'sos image.png', link: 'https://polepellynithishreddy.github.io/SOS_GAME/' },
+  {
+    name: 'TIC-TAC-TOE',
+    image: 'tic-tac-toe image.png',
+    link: '(link unavailable)'
+  },
+  {
+    name: 'CHECKERS',
+    image: 'checkers image.png',
+    link: '(link unavailable)'
+  },
+  {
+    name: 'SOS',
+    image: 'sos image.png',
+    link: '(link unavailable)'
+  },
 ];
 
 games.forEach((game) => {
-	const gameItem = document.createElement('li');
-	gameItem.className = 'game-item';
-	gameItem.innerHTML = `
-		<img src="${game.image}" alt="${game.name}">
-		<h2>${game.name}</h2>
-		<a href="${game.link}">Play Now</a>
-	`;
-	gamesList.appendChild(gameItem);
-});
-
-
+    const gameItem = document.createElement('li');
+    gameItem.className = 'game-item';
+    gameItem.innerHTML = `
+      <img src="${game.image}" alt="${game.name}">
+      <h2>${game.name}</h2>
+      <a href="${game.link}">Play Now</a>
+    `;
+    gamesList.appendChild(gameItem);
+  });
+  
+  // Add event listener for help button
+  const helpBtn = document.getElementById('help-btn');
+  helpBtn.addEventListener('click', () => {
+    const whatsappUrl = 'https://wa.me/qr/XJH27RW34NRDF1>'
+    window.open(whatsappUrl, '_blank');
+  });
+  
